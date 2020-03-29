9ce6ca7d12df216b49ba02dac4ad84722d523225
raziqali0306 <raziqali0306@gmail.com>
2020-03-29 19:52:14 +0530

    removed unnecessary comments

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-// dice = Math.floor(Math.random() * 6) + 1;
-// document.querySelector('#current-' + activePlayer).textContent = dice;
 
 document.querySelector('.dice').style.display = 'none';
 
